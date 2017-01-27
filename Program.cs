93b3fde4214f2af459b298b2760f8e7a7904d3c2
TaHuC <TaHuC@hotmail.com>
2017-01-27 09:33:03 +0200

    Finish Floating Points

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PracticleFloatingPoints
 	{
 		public static void Main(string[] args)
 		{
-			
+			var floating1 = decimal.Parse(Console.ReadLine());
+			var floating2 = double.Parse(Console.ReadLine());
+			var floating3 = decimal.Parse(Console.ReadLine());
+
+			Console.WriteLine("{0} \n {1} \n {2}", floating1, floating2, floating3);
 		}
 	}
 }
